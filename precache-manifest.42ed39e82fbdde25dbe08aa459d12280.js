c7c79ac75a368bc9c060a7da83e9bf1af4d80559
Your Name <you@example.com>
2020-02-07 01:22:40 +0300

    Updates

---
+++ precache-manifest.42ed39e82fbdde25dbe08aa459d12280.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d03a85948813edd0b175c7daee45856e",
+    "url": "/testLoginForm/index.html"
+  },
+  {
+    "revision": "e82b20d96bc168996011",
+    "url": "/testLoginForm/static/js/2.4ea488f4.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/testLoginForm/static/js/2.4ea488f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f931029a9983f69c6485",
+    "url": "/testLoginForm/static/js/main.ffb1eb5d.chunk.js"
+  },
+  {
+    "revision": "37dc39de442fac5edee1",
+    "url": "/testLoginForm/static/js/runtime-main.343ad59e.js"
+  }
+]);
